278892a14848f98965f21fb330ef42346cffdf66
steward <steward831212@yahoo.com.tw>
2017-01-23 15:07:48 +0800

    initial project

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include <stdio.h>
+
+
+int main()
+{
+	printf("Hello World!");
+	system("pause");
+    return 0;
+}
+
